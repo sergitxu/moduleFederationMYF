198a48f1b2a55ac0069a02b763346c3ffe09ea0d
Sergio Ortiz <sergio.ortiz@visma.com>
2023-07-14 16:16:04 +0200

    Add userless lib to mfe1

--- projects/mfe1/src/app/replacement/replacement-list/replacement-list.component.ts
+++ projects/mfe1/src/app/replacement/replacement-list/replacement-list.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AuthLibService } from 'auth-lib';
+import { version } from 'useless-lib';
 
 
 @Component({
@@ -9,5 +10,6 @@ import { AuthLibService } from 'auth-lib';
 export class ReplacementListComponent {
   constructor(private service: AuthLibService) {
     console.log('User Name taken from Shell: ', this.service.user);
+    console.log('Useless lib version in replacement project: ', version);
   }
 }
